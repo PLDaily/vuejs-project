1851ce4b82addb0818c1049092bcb9472ed83104
PLDaily <pcd12321@gmail.com>
2016-12-12 15:42:58 +0800

    通过api.github.com获取个人信息

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -1,14 +1,35 @@
 <template>
 <div id="div1">作者</div><a v-link="'/home'">回到首页</a>
+<img :src="data.avatar_url" />
+<div><span>博客</span>{{data.blog}}</div>
+<div><span>邮箱</span>{{data.email}}</div>
+<div><span>follwering</span>{{data.following}}</div>
+<div><span>start</span>{{data.public_repos}}</div>
+<div><span>flowerer</span>{{data.followers}}</div>
 </template>
 <script>
 export default {
 	data() {
-
+		var data = null;
+		return {
+			data: data
+		}
 	},
 	ready() {
-		console.log($("#div1").attr('id'));
+		var _this = this;
+		$.ajax({
+			url: 'https://api.github.com/users/PLDaily',
+			async: false,
+			dataType: 'jsonp',
+			success: function(results){
+				if(results && results.meta && results.meta.status == 200){
+					console.log(results);
+					_this.data = results.data;
+				}else{
+					console.log('加载失败');
+				}
+			}
+		});
 	}
-	
 }
 </script>
